38af210cf1342bae9df9f39c6214bf0538debdd9
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2023-04-24 18:02:54 -0400

    Update HeaderBlock.js

--- src/components/HeaderBlock.js
+++ src/components/HeaderBlock.js
@@ -1,28 +1,5 @@
 import styled from "styled-components"
 
-const HeaderWrapper = styled.div`
-    font-family: 'Staatliches', cursive;
-    margin: 0rem 8rem 8rem 8rem;
-    background: rgba(255, 255, 255, 0.4);
-    text-align: center;
-    color: white;
-    text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-    padding: 2rem;
-`
-
-const Title = styled.div`
-    font-size: 8rem;
-`
-
-const Subtitle = styled.div`
-    margin-top: 5rem;
-    font-size: 2rem;
-
-    p {
-        margin: 0;
-    }
-`
-
 const HeaderBlock = () => {
     return (
         <HeaderWrapper>
@@ -39,3 +16,26 @@ const HeaderBlock = () => {
 }
 
 export default HeaderBlock;
+
+const HeaderWrapper = styled.div`
+    font-family: 'Staatliches', cursive;
+    margin: 0rem 8rem 0rem 8rem;
+    background: rgba(255, 255, 255, 0.4);
+    text-align: center;
+    color: white;
+    text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    padding: 2rem;
+`
+
+const Title = styled.div`
+    font-size: 8rem;
+`
+
+const Subtitle = styled.div`
+    margin-top: 5rem;
+    font-size: 1.8rem;
+
+    p {
+        margin: 0;
+    }
+`
