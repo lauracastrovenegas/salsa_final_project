50f05e551d00f2293527508c420f91e0883d1e63
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2023-04-24 18:02:57 -0400

    Create PinContainer.js

---
+++ src/components/PinContainer.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+
+
+const PinContainer = () => {
+    return (
+        <Wrapper>
+
+        </Wrapper>
+    )
+}
+
+export default PinContainer;
+
+const Wrapper = styled.div`
+    height: 100vh;
+
+
+`
+
+const Pin = styled.div`
+    
+
+
+`
