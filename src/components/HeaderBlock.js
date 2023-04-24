709ca607955de076132baadc32e02f071d2e02a9
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2023-04-24 17:51:55 -0400

    created headerblock component

---
+++ src/components/HeaderBlock.js
@@ -0,0 +1,41 @@
+import styled from "styled-components"
+
+const HeaderWrapper = styled.div`
+    font-family: 'Staatliches', cursive;
+    margin: 0rem 8rem 8rem 8rem;
+    background: rgba(255, 255, 255, 0.4);
+    text-align: center;
+    color: white;
+    text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    padding: 2rem;
+`
+
+const Title = styled.div`
+    font-size: 8rem;
+`
+
+const Subtitle = styled.div`
+    margin-top: 5rem;
+    font-size: 2rem;
+
+    p {
+        margin: 0;
+    }
+`
+
+const HeaderBlock = () => {
+    return (
+        <HeaderWrapper>
+            <Title>THE SOUNDS OF THE HISPANIC CARIBBEAN</Title>
+            <Subtitle>
+                <p>Columbia University</p>
+                <p>MUSIV2020</p>
+                <p>Laura Castro Venegas</p>
+                <p>Alejandra Maria Nunez</p>
+                <p>Maria Isabel Nunez</p>
+            </Subtitle>
+        </HeaderWrapper>
+    )
+}
+
+export default HeaderBlock;
