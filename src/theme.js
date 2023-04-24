cbeed62c1f7ccb00eda5da54c9e64d01c1acd84e
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2023-04-24 17:51:35 -0400

    Create theme.js

---
+++ src/theme.js
@@ -0,0 +1,17 @@
+const theme = {
+    colors: {
+      white: '#FFFFFF',
+      black: '#000000',
+      blue: "#0066af",
+    },
+    sizes: {
+      mobile: "767px",
+      tablet: "992px",
+      aboveTablet: "993px",
+      aboveLargeTablet: "1024px",
+      desktop: "1200px",
+      largeDesktop: "1440px"
+    }
+  };
+  
+  export default theme;
