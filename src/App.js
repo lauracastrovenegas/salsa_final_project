6982e68835a4079bd72afe6117e0024c0227471f
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2023-04-24 17:52:43 -0400

    added header block to page and made image sticky

--- src/App.js
+++ src/App.js
@@ -1,8 +1,51 @@
+import styled from "styled-components";
+import map from './assets/map.png'
+import theme from "./theme";
+import HeaderBlock from "./components/HeaderBlock";
+import down from './assets/down.svg'
+
+const Wrapper = styled.div`
+  height: 200vh;
+  background-color: ${theme.colors.blue};
+`
+
+const BackgroundImage = styled.div`
+  background-image: url(${map});
+  background-attachment: fixed;
+  padding-top: 4rem;
+  height: 100%;
+  width: 100%;
+  background-position: top left;
+  background-size: contain;
+  background-repeat: no-repeat;
+`
+
+const DownArrow = styled.div`
+  height: 3rem;
+  display: flex;
+  
+  img {
+    height: 100%;
+    object-fit: contain;
+    margin: 0rem auto;
+  }
+
+  svg {
+    color: "#FFFFFF";
+  }
+`
+
+
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Wrapper>
+      <BackgroundImage>
+        <HeaderBlock/>
+        <DownArrow>
+          <img src={down} alt="down arrow"/>
+        </DownArrow>
+      </BackgroundImage>
+    </Wrapper>
   );
 }
 
